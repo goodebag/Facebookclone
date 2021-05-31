f01f66fa0ef2cbc897b2b741b5d0ed637c7b6053
GOODEBAG\Goodebag <Egbule.goodluck@gmail.com>
2021-05-31 14:50:35 +0100

    Maxcolt removed

--- src/Component/Navbar/Navbar.js
+++ src/Component/Navbar/Navbar.js
@@ -1,32 +1,54 @@
 import React, { Component } from 'react';
 import  "./Navbar.css";
 import Grid from '@material-ui/core/Grid';
+import fblogo from '../../images/logo.png';
+import home from '../../images/home.svg';
+import page from '../../images/pages.svg';
+import watch from '../../images/watch.svg';   
+import group from '../../images/groups.svg';
+import market from '../../images/market.svg';
+import { Avatar } from '@material-ui/core';
+
 class Navbar extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {  } 
     }
     render() { 
         return (
             <div >
-              <Grid container>
+              <Grid container className="navbar_main">
                         <Grid item xs ={3}>
-                            left
-                            </Grid> 
+                                    <div className = "navbar_left">
+                                    <img className = "navbar_logo" src={fblogo} width = "40px" ></img> 
+                                    <input className ="navbar_search" type = "Text" placeholder ="Search facebook"/>
+                                    </div>
+                        </Grid> 
 
 
                             <Grid item xs ={6}>
-                            center
+                                <div className = "navbar_Container">
+                                <div className = "navbar_tabs active"> <img  src ={home} height ="35px" width ="35px"></img>  </div>
+                                <div className = "navbar_tabs"> <img src ={page} height ="35px" width ="35px"></img>  </div>
+                                <div className = "navbar_tabs"> <img  src ={watch} height ="35px" width ="35px"></img>  </div>
+                                <div className = "navbar_tabs"> <img src ={market} height ="35px" width ="35px"></img>  </div>
+                                <div className = "navbar_tabs"> <img  src ={group} height ="35px" width ="35px"></img>  </div>
+                                </div>
                             </Grid>
 
-
+ 
                             <Grid item xs ={3}>
-                            right
+                                    <div className ="navbar_right">
+                                        <di className ="navbar_righttab">
+                                            <Avatar className="navbar_right_img" src =""/>
+                                            <div className= "navbar_profilename"> Username</div>
+                                        </di>
+                                    </div>
                             </Grid> 
 
-                  </Grid> 
+                  </Grid>  
             </div>
-          );
+          ); 
     }
 }
  
